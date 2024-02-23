77c74a86983d8aa56e8dd977ad4206646f4dd2db
Michael Friel <mikefriel92@hotmail.com>
2024-02-23 19:21:41 +0000

    Created the Employee class and extended it with Manager

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-const inquirer = require("inquirer");
+const inquirer = require("inquirer")
 const path = require("path");
 const fs = require("fs");
 
@@ -10,6 +10,6 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
 
-
+console.log("Test this!")
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
